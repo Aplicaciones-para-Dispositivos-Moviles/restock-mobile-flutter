6a730159c4af4ebb7108c2c20ee0dd000dc6d374
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-12-05 05:53:45 -0500

    feat: add new routes to main.

--- restock/lib/main.dart
+++ restock/lib/main.dart
@@ -1,19 +1,53 @@
-import 'package:restock/features/auth/data/auth_service.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:restock/features/auth/data/local/auth_storage.dart';
+import 'package:restock/features/auth/data/remote/auth_service.dart';
 import 'package:restock/features/auth/presentation/blocs/login_bloc.dart';
 import 'package:restock/features/auth/presentation/blocs/register_bloc.dart';
 import 'package:restock/features/auth/presentation/pages/login_page.dart';
 import 'package:restock/features/home/presentation/pages/home_page.dart';
 
-void main() {
+// INVENTORY
+import 'package:restock/features/resource/inventory/data/remote/inventory_service.dart';
+import 'package:restock/features/resource/inventory/data/repositories/inventory_repository_impl.dart';
+import 'package:restock/features/resource/inventory/presentation/blocs/inventory_bloc.dart';
+import 'package:restock/features/resource/inventory/presentation/pages/inventory_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final authService = AuthService();
+  final authStorage = AuthStorage();
+
+  final inventoryService = InventoryService();
+  final inventoryRepository = InventoryRepositoryImpl(
+    service: inventoryService,
+    getUserId: () async {
+      final id = await authStorage.getUserId();
+      if (id == null) {
+        throw Exception('No hay usuario logueado');
+      }
+      return id;
+    },
+  );
 
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => LoginBloc(service: authService)),
-        BlocProvider(create: (_) => RegisterBloc(service: authService)),
+        BlocProvider(
+          create: (_) => LoginBloc(
+            service: authService,
+            storage: authStorage,
+          ),
+        ),
+        BlocProvider(
+          create: (_) => RegisterBloc(service: authService),
+        ),
+        BlocProvider(
+          create: (_) => InventoryBloc(repository: inventoryRepository),
+        ),
       ],
       child: const RestockApp(),
     ),
@@ -27,12 +61,12 @@ class RestockApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home: const LoginPage(),
-
+      title: 'Restock',
+      initialRoute: '/login',
       routes: {
         '/login': (_) => const LoginPage(),
         '/home': (_) => const HomePage(),
+        '/inventory': (_) => const InventoryPage(),
       },
     );
   }
