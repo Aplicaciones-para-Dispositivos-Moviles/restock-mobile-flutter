d0aa0cd4b31edd67081aae62bfaa3b2c6c25c136
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-12-05 03:49:48 -0500

    feat: change api constans to production.

--- restock/lib/core/constants/api_constants.dart
+++ restock/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'https://restock-platform.onrender.com/';
 
   static const String loginEndpoint = '/api/v1/authentication/sign-in';
   static const String registerEndpoint = '/api/v1/authentication/sign-up';
